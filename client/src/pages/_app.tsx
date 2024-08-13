916541db2422907dafb293f70f4064ea9a990f91
dahoon06 <dahoon226@naver.com>
2024-08-13 16:51:29 +0900

    feat: Remove getServerSideProps in app

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -3,17 +3,12 @@ import { ReactElement, useState } from "react";
 import { queryClient } from "@lib/tanstack";
 import { GlobalStyle, theme } from "@styles/styled-components";
 import { ThemeProvider } from "styled-components";
-import {
-  dehydrate,
-  HydrationBoundary,
-  QueryClientProvider,
-} from "@tanstack/react-query";
+import { HydrationBoundary, QueryClientProvider } from "@tanstack/react-query";
 import { ToastHandler } from "@components/common/toast";
-import { GetServerSideProps, GetServerSidePropsContext } from "next";
 import { BaseLayout } from "@layouts/BaseLayout";
 import "@styles/sass/index.scss";
 
-const App = ({ Component, pageProps }: AppProps): ReactElement => {
+const DottoApp = ({ Component, pageProps }: AppProps): ReactElement => {
   const [queryState] = useState(() => queryClient);
   return (
     <>
@@ -33,20 +28,4 @@ const App = ({ Component, pageProps }: AppProps): ReactElement => {
   );
 };
 
-export const getServerSideProps: GetServerSideProps = async (
-  ctx: GetServerSidePropsContext
-) => {
-  try {
-    return {
-      props: {
-        dehydratedState: dehydrate(queryClient),
-      },
-    };
-  } catch (e) {
-    return {
-      props: {},
-    };
-  }
-};
-
-export default App;
+export default DottoApp;
--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,5 +1,8 @@
 import { Banner } from "@components/banner/Banner";
 import { DottoView } from "@components/dotto/DottoView";
+import { GetServerSideProps, GetServerSidePropsContext } from "next";
+import { dehydrate } from "@tanstack/react-query";
+import { queryClient } from "@lib/tanstack";
 
 export default function MainPage() {
   return (
@@ -9,3 +12,19 @@ export default function MainPage() {
     </>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async (
+  ctx: GetServerSidePropsContext
+) => {
+  try {
+    return {
+      props: {
+        dehydratedState: dehydrate(queryClient),
+      },
+    };
+  } catch (e) {
+    return {
+      props: {},
+    };
+  }
+};
