06727815a9ebd713ed5b5fb3d731878ccc38ba70
dahoon06 <dahoon226@naver.com>
2024-08-15 16:41:25 +0900

    feat: Update Menu list item

--- client/src/components/header/MenuList.tsx
+++ client/src/components/header/MenuList.tsx
@@ -1,35 +1,77 @@
-import {ReactElement} from "react";
+import { ReactElement, useCallback } from "react";
 import styled from "styled-components";
 import Link from "next/link";
+import { Typography } from "@components/common/typograhpy";
+import classNames from "classnames";
+import { useRouter } from "next/router";
 
 const MenuLayout = styled.nav`
-    width: 100%;
-    
-    ul {
-        display: flex;
-        flex: 1;
-        list-style: none;
-        gap: 30px;
+  width: 100%;
+
+  ul {
+    display: flex;
+    flex: 1;
+    list-style: none;
+    gap: 30px;
+
+    li {
+      .item {
+        color: #919191;
+
+        &.active {
+          color: #222;
+        }
+      }
     }
+  }
 `;
 
 export const MenuList = (): ReactElement => {
+  const router = useRouter();
+
+  const isActive = useCallback(
+    (path: string): boolean => {
+      if (path === "/") {
+        return router.pathname === path;
+      }
+      return router.pathname.startsWith(path);
+    },
+    [router.pathname]
+  );
   return (
     <MenuLayout>
       <ul>
         <li>
-          <Link href={'/'}>
-            HOME
+          <Link href={"/"}>
+            <Typography
+              as={"span"}
+              className={classNames("item", { active: isActive("/") })}
+            >
+              HOME
+            </Typography>
           </Link>
         </li>
         <li>
-          <Link href={'/design'}>
-            닷투 게시판
+          <Link href={"/design"}>
+            <Typography
+              as={"span"}
+              className={classNames("item", { active: isActive("/design") })}
+            >
+              닷투 게시판
+            </Typography>
           </Link>
         </li>
-        <li>고객지원</li>
+        <li>
+          <Link href={"/faq"}>
+            <Typography
+              as={"span"}
+              className={classNames("item", { active: isActive("/faq") })}
+            >
+              고객지원
+            </Typography>
+          </Link>
+        </li>
       </ul>
     </MenuLayout>
-
-  )
-}
+  );
+};
